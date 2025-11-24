31cdd6173dbe4d9da1330df34af6545f0098ec53
notolite <mina37s.sumomo@gmail.com>
2025-11-24 10:30:47 +0900

    trial

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 javascript:(function(){
   const frmst = document.querySelector("frameset");
-  frmst.removeAttribute("cols");
+  frmst.setAttribute("cols", "*");
   frmst.setAttribute("rows", "300,*");
+  const brs = frames["yui"].document.querySelectorAll("br");
+  const rms = [0, 2, 3, 4, 5, 6, 8, 8, 10, 11];
+  for (let i = 0; i < rms.length; i++) {
+    brs[rms[i]].remove();
+  }
 })();
